c7b87a52ca0a94efc8e7a547573b6b99a689da35
中豪 <zhonghao2019@aliyun.com>
2019-06-11 23:13:39 +0800

    api

---
+++ microservice-cloud-02-api/src/main/java/com/zhonghao/springcloud/entities/Product.java
@@ -0,0 +1,47 @@
+package com.zhonghao.springcloud.entities;
+
+import java.io.Serializable;
+
+public class Product implements Serializable {
+
+    private Long pid;
+    private String productName;
+    private String  dbSource;
+
+    public Product() {
+    }
+
+    public Product(String productName) {
+        this.productName = productName;
+    }
+
+    public Product(Long pid, String productName, String dbSource) {
+        this.pid = pid;
+        this.productName = productName;
+        this.dbSource = dbSource;
+    }
+
+    public Long getPid() {
+        return pid;
+    }
+
+    public void setPid(Long pid) {
+        this.pid = pid;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public String getDbSource() {
+        return dbSource;
+    }
+
+    public void setDbSource(String dbSource) {
+        this.dbSource = dbSource;
+    }
+}
