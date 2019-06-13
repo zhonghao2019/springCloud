f41211b79ce5dee28223aa577f180143bdd37272
中豪 <zhonghao2019@aliyun.com>
2019-06-13 23:16:26 +0800

    服务消费者

---
+++ microservice-cloud-04-consumer-product-80/src/main/java/com/zhonghao/springcloud/ProductConsumer_80.java
@@ -0,0 +1,14 @@
+package com.zhonghao.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductConsumer_80 {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(ProductConsumer_80.class, args);
+    }
+
+}
---
+++ microservice-cloud-04-consumer-product-80/src/main/java/com/zhonghao/springcloud/controller/ProductController_Consumer.java
@@ -0,0 +1,35 @@
+package com.zhonghao.springcloud.controller;
+
+import com.zhonghao.springcloud.entities.Product;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+
+@RestController
+public class ProductController_Consumer {
+
+    private static final String REST_URL_PREFIX = "http://localhost:8001";
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @RequestMapping(value = "/consumer/product/add")
+    public boolean add(Product product) {
+        return restTemplate.postForObject(REST_URL_PREFIX + "/product/add", product, Boolean.class);
+    }
+
+    @RequestMapping(value = "/consumer/product/get/{id}")
+    public Product get(@PathVariable("id") Long id) {
+        return restTemplate.getForObject(REST_URL_PREFIX + "/product/get/" + id, Product.class);
+    }
+
+    @RequestMapping(value = "/consumer/product/list")
+    public List<Product> list() {
+        return restTemplate.getForObject(REST_URL_PREFIX + "/product/list", List.class);
+    }
+
+}
